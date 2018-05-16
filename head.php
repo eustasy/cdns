2edfffe6eefd8f574ab6f3d3b6ddbbc6efba71cc
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-16 22:30:50 +0100

    Update head.php

--- head.php
+++ head.php
@@ -23,6 +23,6 @@
 		<link rel="canonical" href="https://labs.eustasy.org/cdns/'.$Canonical.'">'; ?>
 
 		<link rel="stylesheet prefetch" href="https://fonts.googleapis.com/css?family=Lusitana|Roboto:400,500,400italic|Source+Code+Pro" data-noprefix>
-		<link rel="stylesheet prefetch" href="https://cdn.jsdelivr.net/combine/gh/eustasy/Colors.css@1.9.2/colors.min.css,gh/necolas/normalize.css@8.0.0/normalize.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/gh/eustasy/Colors.css@1/colors.min.css,gh/necolas/normalize.css@8/normalize.min.css">
 		<link rel="stylesheet prefetch" href="styles.css">
 	</head>
